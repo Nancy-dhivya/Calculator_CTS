bbddd815c7b828ed7da6a385895830e8e5d2e569
Sowmiya Arun <107838488+sowmiya2383@users.noreply.github.com>
2024-04-04 22:32:37 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,3 +1,18 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
+
 from calculator import add, subtract, multiply, divide
 
 print("Select operation:")
